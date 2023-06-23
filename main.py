32380a264628d5b720cbd556f8bdf53fb8cfda2b
Jaden1005 <68866715+Jaden1005@users.noreply.github.com>
2023-06-22 21:22:48 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+
+from storage import all_articles, liked_articles, not_liked_articles
+from demographic_filtering import output
+from content_filtering import get_recommendations
+
+app = Flask(__name__)
+
+@app.route("/get-article")
+def get_article():
+    article_data = {
+        "url": all_articles[0][11],
+        "title": all_articles[0][12],
+        "text": all_articles[0][13],
+        "lang": all_articles[0][14],
+        "total_events": all_articles[0][15]
+    }
+    return jsonify({
+        "data": article_data,
+        "status": "success"
+    })
+
+@app.route("/liked-article", methods=["POST"])
+def liked_article():
+    article = all_articles[0]
+    liked_articles.append(article)
+    all_articles.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/unliked-article", methods=["POST"])
+def unliked_article():
+    article = all_articles[0]
+    unliked_articles.append(article)
+    all_articles.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/popular-article", methods=["POST"])
+def popular_article():
+    article = all_articles[0]
+    popular_articles.append(article)
+    all_articles.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/recommended-article", methods=["POST"])
+def recommended_article():
+    article = all_articles[0]
+    recommended_articles.append(article)
+    all_articles.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+
+
+if __name__ == "__main__":
+    app.run()
